9aa534b2aa31c259bf594f50def755caa2ba2a96
clement-elbaz <clem.ds@gmail.com>
2014-08-15 01:50:36 +0200

    script debug

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-java -DSTOP.PORT=8079 -DSTOP.KEY=somedamnmusic_rulezzzzz -jar start.jar --stop
+java -DSTOP.PORT=8079 -DSTOP.KEY=somedamnmusic_rulezzzzz -jar /home/jetty/start.jar --stop
 cp target/somedamnmusic.war /home/jetty/webapps/root.war
-java -DSTOP.PORT=8079 -DSTOP.KEY=somedamnmusic_rulezzzzz -jar start.jar &
+java -DSTOP.PORT=8079 -DSTOP.KEY=somedamnmusic_rulezzzzz -jar /home/jetty/start.jar &
